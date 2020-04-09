cbeffd13f040331d3f56995549c2841add7b503c
Soundium <43785888+Soundium@users.noreply.github.com>
2020-04-09 10:35:12 +0200

    Update whitelist.sh

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -6,7 +6,7 @@
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
 PIHOLE_LOCATION="/etc/pihole"
-GRAVITY_UPDATE_COMMAND="pihole -g"
+GRAVITY_UPDATE_COMMAND="pihole -w -q"
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
 echo -e "\n"
@@ -26,8 +26,7 @@ echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -w -q $(cat /etc/pihole/whitelist.txt | xargs)
-${GRAVITY_UPDATE_COMMAND} > /dev/null
+${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
  
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
